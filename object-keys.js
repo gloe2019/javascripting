6547aaa3566a6bbd0838922eb7a20f09a78263ae
Gloria <gloedem@gmail.com>
2021-07-07 14:36:25 -0400

    completed objects module...

---
+++ object-keys.js
@@ -0,0 +1,8 @@
+const car = {
+  make: "Honda",
+  model: "Accord",
+  year: 2020,
+};
+//Object.keys(obj_name) allows you list all the avaiable keys of an object. this could be useful for looping though properties and manipulating their values...
+const keys = Object.keys(car);
+console.log(keys);
